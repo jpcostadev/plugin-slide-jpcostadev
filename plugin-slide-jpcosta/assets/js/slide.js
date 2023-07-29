7f7bb8af0b19e4906d38725749129217aca9ab73
Pedro <104322778+jpcostadev@users.noreply.github.com>
2023-07-29 12:30:56 -0300

    distancias

--- plugin-slide-jpcosta/assets/js/slide.js
+++ plugin-slide-jpcosta/assets/js/slide.js
@@ -2,21 +2,38 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    };
   }
 
   onStart(event) {
     event.preventDefault();
+    this.dist.startX = event.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
+    console.log(this.dist.startX);
+  }
 
-    console.log(this);
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 2.2;
+    console.log(this.dist.movement);
+    return this.dist.finalPosition - this.dist.movement;
   }
 
   onMove(event) {
-    console.log("moveu");
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
-    console.log("Acabou");
+    this.dist.finalPosition = this.dist.movePosition;
     this.wrapper.removeEventListener("mousemove", this.onMove);
   }
 
